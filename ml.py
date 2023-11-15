8047094ab62374a834ce3de3e79f9cb174d0e4a4
lakshmi jinkala <142764159+laxthelaxy@users.noreply.github.com>
2023-11-14 22:13:41 -0600

    installing libraries ml.py

---
+++ ml.py
@@ -0,0 +1,14 @@
+#libraries used for data analysis in machine learning
+
+!pip install numpy
+import numpy as np
+
+!pip install pandas 
+import pandas as pd 
+
+!pip install matplotlib
+import matplotlib.pyplot as plt
+
+!pip install sklearn
+from sklearn import datasets, model_selection 
+
